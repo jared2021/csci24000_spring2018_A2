27087fd8da2085feda5ce3a145adf718200b2b9e
Schoolcraft, Jared <jascho@iu.edu>
2018-02-01 16:23:44 -0500

    Create Race.cpp
    Copied file and code from Andy's files on Canvas

---
+++ Race.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
+#define RACE_LENGTH 50
+
+void advanceTortoise(int* ptrTortoise);
+void advanceHare(int* ptrHare);
+void printPosition(int* ptrTortoise, int* ptrHare);
+int main()
+{
+
+}
