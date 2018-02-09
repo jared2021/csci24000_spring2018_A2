6767b49d7fc73e0edff1aa0f210f50d5d5336813
Jared Schoolcraft <jascho@tesla.cs.iupui.edu>
2018-02-09 11:50:32 -0500

    fifth and final commit putall of my function builds outside of my main function and fixed problems I was having with my random number function and printing the results

--- Race.cpp
+++ Race.cpp
@@ -9,50 +9,16 @@ void advanceHare(int* ptrHare);
 void printPosition(int* ptrTortoise, int* ptrHare);
 
 int main()
-{int *ptrHare=0;
- int *ptrTortoise=0;
+{srand(time(NULL));	
+ int H=0;
+ int T=0;
+ int* ptrHare=&H;
+ int* ptrTortoise=&T;
 //we'll be doing essentially the same thing as in the first assignment except instead of using an array we will be using a pointer	
-	while (*ptrHare,*ptrTortoise <49){
-		advanceHare( ptrHare);{
-//advanceHare function
-			srand(time(NULL));
-			int randH=rand()%10+1;
-			if (randH<4){
-				*ptrHare=*ptrHare+2;
-			}
-			if (randH=4,5,6){
-				*ptrHare=*ptrHare+3;
-			}
-			if (randH=7){
-				*ptrHare=*ptrHare+1;
-			}
-			if (randH=8){
-				*ptrHare=*ptrHare-2;
-			}
-			if (randH=9,10){
-				*ptrHare=*ptrHare-3;
-			}
-		}
-//advanceHare function ends here
-		advanceTortoise( ptrTortoise );{
-//advanceTortoise function
-			srand(time(NULL));
-			int randT=rand()%10+1;
-			if (randT<7){
-				*ptrTortoise=*ptrTortoise+1;
-			}
-			if (randT=7){
-				*ptrTortoise=*ptrTortoise+2;
-			}
-			if (randT=8){
-				*ptrTortoise=*ptrTortoise+3;
-			}
-			if (randT>8){
-				*ptrTortoise=*ptrTortoise;
-			}
-		}
-//advanceTortoise function ends here
-//if statements to enforce rules for the race	
+	while (*ptrHare<50&&*ptrTortoise <50){
+		advanceHare(ptrHare);
+		advanceTortoise(ptrTortoise);
+		//if statements to enforce rules for the race	
 		if (*ptrHare>50){
 			*ptrHare=50;
 		}
@@ -65,25 +31,76 @@ int main()
 		if(*ptrTortoise<0){
 			*ptrTortoise=0;
 		}
-		if (*ptrHare=*ptrTortoise){
+		if (*ptrHare==*ptrTortoise){
 			*ptrTortoise=*ptrTortoise-1;
 		}
 //if statements ends
-//printPosition function starts
-		printPosition(ptrHare,ptrTortoise);{
-			std::cout <<std::setw(RACE_LENGTH)<<std::left;
-			std::cout <<std::left<<(*ptrHare)<<'H';
-			std::cout <<std::left<<(*ptrTortoise)<<'T';
-			}
-//printPosition function ends		
-	}
-	if (*ptrHare=50){
+		printPosition(ptrHare,ptrTortoise);
+		}
+	if (*ptrHare==50){
 		std::cout<<"The Hare won the race.";
 	}
-	if (*ptrTortoise=50){
+	else if (*ptrTortoise==50){
 		std::cout<<"The Tortoise won the race.";
 	}
 	else{
 		std::cout<<"One eternity later...no one made it to the finsih line.";
 	}
+	
 }
+//main function ends
+ void advanceHare(int* ptrHare){
+//advanceHare function
+			int randH;
+			randH=rand()%10+1;
+			if (randH<4){
+				*ptrHare=*ptrHare+2;
+			}
+			if (randH==4||randH==5||randH==6){
+				*ptrHare=*ptrHare+3;
+			}
+			if (randH==7){
+				*ptrHare=*ptrHare+1;
+			}
+			if (randH==8){
+				*ptrHare=*ptrHare-2;
+			}
+			if (randH==9||randH==10){
+				*ptrHare=*ptrHare-3;
+			}
+		}
+//advanceHare function ends here
+void advanceTortoise(int* ptrTortoise ){
+//advanceTortoise function
+			int randT;
+			randT=rand()%10+1;
+			if (randT<7){
+				*ptrTortoise=*ptrTortoise+1;
+			}
+			if (randT==7){
+				*ptrTortoise=*ptrTortoise+2;
+			}
+			if (randT==8){
+				*ptrTortoise=*ptrTortoise+3;
+			}
+			if (randT>8){
+				*ptrTortoise=*ptrTortoise;
+			}
+		}
+//advanceTortoise function ends here
+//printPosition function starts
+void printPosition(int* ptrHare,int* ptrTortoise){
+	if(*ptrHare>*ptrTortoise){
+		std::cout<<std::setw(*ptrTortoise)<<'T';
+		std::cout<<std::setw(*ptrHare-*ptrTortoise)<<'H';
+		std::cout<<std::setw(RACE_LENGTH-*ptrHare+1)<<'|'<<'\n';
+	}
+	else if(*ptrTortoise>*ptrHare){
+		std::cout<<std::setw(*ptrHare)<<'H';
+		std::cout<<std::setw(*ptrTortoise-*ptrHare)<<'T';
+		std::cout<<std::setw(RACE_LENGTH-*ptrTortoise+1)<<'|'<<'\n';
+	}
+			}
+//printPosition function ends		
+
+
